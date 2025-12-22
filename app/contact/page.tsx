4a74164b9dca8b57ad3c75c3b49010638e7a3378
rk <khanri1@outlook.com>
2025-12-22 01:01:05 +0000

    contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,37 @@
+const contactDetails = [
+  // include image / icon
+  {
+    id: 1,
+    type: "Email",
+    detail: "email@mail.com",
+    href: "mailto:email@mail.com",
+  },
+  { id: 1, type: "Phone", detail: "1234567", href: "tel: 1234567" },
+  { id: 1, type: "Social media", detail: "_handle", href: "https://www..." },
+  { id: 1, type: "", detail: "", href: "" },
+];
 export default function ContactPage() {
-  return <div>Contact Page</div>;
+  return (
+    <div className="flex flex-col gap-5">
+      <h1 className="">Contact</h1>
+      <div className="flex flex-col gap-5 flex-wrap">
+        {contactDetails.map((contact) => {
+          return (
+            <div
+              key={contact.id}
+              className="flex group bg-blue-200 rounded p-5 md:w-fit hover:bg-dark-green"
+              id="contact-card"
+            >
+              <a href={contact.href} target="_blank" rel="noopener noreferrer">
+                <div className="flex group-hover:text-white">
+                  <p className="flex font-bold">{contact.type}:&nbsp;</p>
+                  <p className="flex">{contact.detail}</p>
+                </div>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
