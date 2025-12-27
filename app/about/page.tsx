9dc9ebf6eb52cea44810a817930d76dc314ac016
rk <khanri1@outlook.com>
2025-12-27 23:58:51 +0000

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,32 @@
 export default function AboutPage() {
-  return <div>About Page</div>;
+  const aboutMeText =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<br.> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.<br/> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+  const AboutMe = aboutMeText.split("<br/>");
+
+  const newText = [];
+
+  return (
+    <div className="flex flex-col gap-5 p-10">
+      <h1>About</h1>
+      <div>
+        <article className="">
+          <img
+            src="assets/all-about-me.png"
+            alt="profile photo"
+            className="float-right h-80 w-80"
+          ></img>
+          <div className="">
+            {AboutMe.map((paragraph) => {
+              return (
+                <p key={paragraph} className="flex pt-5">
+                  {paragraph}
+                </p>
+              );
+            })}
+          </div>
+        </article>
+      </div>
+    </div>
+  );
 }
