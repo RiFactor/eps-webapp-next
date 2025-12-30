64085a2d81480278798cf5cd341127e59054109c
rk <khanri1@outlook.com>
2025-12-30 10:36:59 +0000

    carousel style

--- app/image-gallery/image.tsx
+++ app/image-gallery/image.tsx
@@ -15,8 +15,7 @@ export default function CarouselImage({ index, status }: IProps) {
       // fix height / size?! h-96 is ok
       //   className="flex flex-col items-center rounded justify-center bg-blue-200 carousel-image"
       //   ${status === "current" && }
-      className={`h-90
-        "flex flex-col transition transform ease-in-out duration-1000 carousel-image"`}
+      className="flex flex-col w-1/3 justify-between" // add gap
       //   transition: transform 0.5s ease-in-out;
     >
       <img
@@ -25,13 +24,16 @@ export default function CarouselImage({ index, status }: IProps) {
         // look at responsive cropping photo...
         // className="flex h-80 items-center bg-red-200 p-5 transition ease-in-out delay-300 motion-reduce:transition-none"
         // className="flex w-100% h-auto rounded"
-        className="flex w-100% h-auto rounded"
+        className={`flex object-contain  ${
+          status === "current" ? "h-80" : "h-50"
+        }`} // make it rounded
+        // className={`${!status && "opacity-50"} "flex object-contain h-80"`}
         // w-20 h-20
         // fixed md:top-0 md:right-0
       ></img>
-      <p className="flex p-5 rounded card">
+      {/* <p className="flex justify-center">
         {photoGallery[index]?.alt} {photoGallery[index].id}
-      </p>
+      </p> */}
     </div>
   );
 }
