23771643c070e0cb425a52cde5c35bd312b70d13
rk <khanri1@outlook.com>
2025-12-30 22:26:13 +0000

    services list

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,17 @@
 const ServicesPage = () => {
-  return <div>Services and what we offer</div>;
+  return (
+    <div>
+      Services and what we offer
+      <ul>Services</ul>
+      <ol>Gutter Cleaning</ol>
+      <ol>Guttering Clearing</ol>
+      <ol>Roof and solar panel cleaning</ol>
+      <ol>Driveway Cleaning</ol>
+      <ol>Patio Cleaning</ol>
+      <ol>Residential Jet Washing</ol>
+      <ol>Commercial Pressure Cleaning</ol>
+    </div>
+  );
 };
 
 export default ServicesPage;
